ed7cc1749d1061b90466670604fec7cc80df65d5
for-retail <narutohina1411@gmail.com>
2026-02-13 09:50:23 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,19 +1,27 @@
 const CACHE="gaa-player-v2";
 
 self.addEventListener("install",e=>{
+self.skipWaiting();
 e.waitUntil(
-caches.open(CACHE).then(c=>
-c.addAll([
-"./",
-"./index.html",
-"./manifest.json"
-])
-)
+caches.open(CACHE).then(cache=>{
+return cache.addAll(["./","./index.html"]);
+})
+);
+});
+
+self.addEventListener("activate",e=>{
+e.waitUntil(
+caches.keys().then(keys=>{
+return Promise.all(
+keys.filter(k=>k!==CACHE)
+.map(k=>caches.delete(k))
+);
+})
 );
 });
 
 self.addEventListener("fetch",e=>{
 e.respondWith(
-caches.match(e.request).then(r=>r||fetch(e.request))
+caches.match(e.request).then(res=>res||fetch(e.request))
 );
 });
